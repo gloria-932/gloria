7477032ec665211ec51071161b025a8a7673c3b5
gloria-932 <glorianambuya932@gmail.com>
2025-01-25 11:35:50 +0300

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,82 @@
+/*alert("smile")
+prompt("hi you")
+confirm("cease")
+document.getElementById("marry").onclick=function(){
+    alert("item bought")
+}*/
+
+/*document.getElementById("marry").onclick=function(){
+    document.getElementById("marry").innerHTML="You Have Been Scammed"
+}*/
+
+document.getElementById("marry").onclick=function(){
+    document.getElementById("hey").src="./Assets/Hi.jpg"
+}
+
+
+document.getElementById("marry").addEventListener("click",function(){
+    document.getElementById("marry").style.background="red"
+    document.getElementById("marry").style.fontSize="40px"
+})
+
+/*let me=5
+alert(me)*/
+
+function heyyou(){
+    let me=6
+    alert(me)
+}
+//heyyou(me)
+
+/*let a=5
+if(a===6){
+alert(true)
+}*/
+
+/*let a=6
+if(a===5){
+    alert(true)
+}else{
+    alert(false)
+}*/
+/*let a;
+
+a=1;
+
+switch (a) {
+    case a=5:alert("5")
+        
+        break;
+
+    case a=4:alert("4")
+        
+        break;
+    
+    case a=3:alert("3")
+        
+        break;
+    
+    case a=2:alert("2")
+        
+        break;
+
+    case a=1:alert("1")
+        
+        break;
+
+    case a=0:alert("0")
+        
+        break;
+
+    default:alert("Not a Number")
+        break;
+}*/
+const A=[1,2,3,4,5,6]
+/*for (let A = 0; A < array.length; A++) {
+    const element = array[A];
+    alert(element)
+    
+}*/
+A.forEach(element => {
+    //(element)
+});
